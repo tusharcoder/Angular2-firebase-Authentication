e9f695b18b5e5b8d61ef118d6465c19339615a93
tushar_scaledesk <tushar@scaledesk.com>
2017-01-28 00:44:37 +0530

    add the google, facebook and the github logins

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,17 @@
+/**
+* @Author: Tushar Agarwal(tusharcoder) <tushar>
+* @Date:   2017-01-27T23:45:07+05:30
+* @Email:  tamyworld@gmail.com
+* @Filename: app.component.ts
+* @Last modified by:   tushar
+* @Last modified time: 2017-01-28T00:38:10+05:30
+*/
+
+
+
 import { Component } from '@angular/core';
+import { AngularFire, AuthProviders  } from 'angularfire2';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +19,60 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+title = 'app works';
+user=null;
+logged_in=false;
+provider=null;
+constructor(private af: AngularFire) {
+    this.af.auth.subscribe(user=>{
+      if(user){
+        this.user=user;
+      }else{
+        this.user={}
+      }
+    })
+  }
+
+  googlelogin(){
+    this.af.auth.login({
+      provider:AuthProviders.Google,
+    }).then((data:any)=>{
+      this.user=data;
+      console.log(this.user);
+      this.provider="Google";
+      this.logged_in=true;
+    });
+  }
+
+  facebooklogin(){
+    this.af.auth.login({
+      provider:AuthProviders.Facebook,
+    }).then((user)=>{
+      this.user=user;
+      console.log(this.user);
+      this.provider="Facebook";
+      this.logged_in=true;
+    });
+  }
+
+  githublogin(){
+    this.af.auth.login({
+      provider:AuthProviders.Github,
+    }).then((user:any)=>{
+      this.user=user;
+      console.log(this.user);
+      this.provider="Github";
+      this.logged_in=true;
+    });
+  }
+
+  logout(){
+    this.af.auth.logout().then(()=>{
+      this.logged_in=false;
+      this.user={};
+      this.provider=null;
+      alert("you are successfully logged out");
+    });
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,30 @@
+/**
+* @Author: Tushar Agarwal(tusharcoder) <tushar>
+* @Date:   2017-01-27T23:42:51+05:30
+* @Email:  tamyworld@gmail.com
+* @Filename: app.module.ts
+* @Last modified by:   tushar
+* @Last modified time: 2017-01-28T00:43:28+05:30
+*/
+
+
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import {AngularFireModule, AuthProviders, AuthMethods} from 'angularfire2';
 
+	  // Initialize Firebase
+	  export const firebaseconfig = {
+	      apiKey: "AIzaSyBYpAy1_s_8OL4N3v8bfoqnyTmZMmRpZa4",
+	          authDomain: "authentication-38726.firebaseapp.com",
+		      databaseURL: "https://authentication-38726.firebaseio.com",
+		          storageBucket: "authentication-38726.appspot.com",
+			      messagingSenderId: "449467048428"
+			   };
 @NgModule({
   declarations: [
     AppComponent
@@ -12,7 +32,11 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseconfig,{
+			provider:AuthProviders.Google,
+			method:AuthMethods.Popup
+		}),
   ],
   providers: [],
   bootstrap: [AppComponent]
